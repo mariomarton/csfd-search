101a71a0182dc0f44c3cc8259a8ffe69fb8d0f84
mariomarton <mariomarton@icloud.com>
2025-04-25 16:07:46 +0200

    [models] add Actor, Film models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    id = models.AutoField(primary_key=True)  # auto-generated if omitted, made explicit here for readability
+    csfd_id = models.IntegerField(unique=True, db_index=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Film(models.Model):
+    id = models.AutoField(primary_key=True)  # auto-generated if omitted, made explicit here for readability
+    csfd_id = models.IntegerField(unique=True, db_index=True)
+    title = models.CharField(max_length=255)
+    actors = models.ManyToManyField(Actor, related_name='films')
+
+    def __str__(self):
+        return self.title
